79c7d69cf2b72b9ebb49843e30bc4d797c900f10
Cameron Durham <u64.cam@gmail.com>
2022-06-13 21:34:15 -0700

    Add doc comments

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,29 @@ impl FromResidual<Yeet<LolCatErr>> for LolCatErr {
 
 }
 
+/// # trying yeet in rust nightly
+/// Seeing if it really works from:
+///     [This is why Rust is better than C++](https://youtu.be/LpKm2GjIf5Y)
+///
+/// ```sh
+/// rustup default nightly
+/// cargo run
+///    Compiling yeet-expr v0.1.0
+///     Finished dev [unoptimized + debuginfo] target(s) in 0.40s
+///      Running `target/debug/yeet-expr`
+/// Error: Lmao
+/// ```
+///
+/// ## links
+///
+/// 1. [rust-lang/rust #96374](https://github.com/rust-lang/rust/issues/96374)
+/// 2. [rust unstable book](https://doc.rust-lang.org/beta/unstable-book/language-features/yeet-expr.html)
+/// 3. [try trait v2](https://rust-lang.github.io/rfcs/3058-try-trait-v2.html)
+/// 4. [tracking issue for `ops::Yeet`](https://github.com/rust-lang/rust/issues/96374)
+///
+/// -----
+///
+
 fn main() -> Result<(), LolCatErr> {
 
     fn foo() -> Result<String, i32> {
